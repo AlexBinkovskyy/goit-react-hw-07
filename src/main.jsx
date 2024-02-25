2e79f287ca5f97c9bed898ea14a76aeb5cfce5db
AlexBinkovskyy <alex_bin@ukr.net>
2024-02-25 19:09:08 +0200

    ss

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,15 @@ import { App } from './components/App.jsx';
 import 'modern-normalize';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistedStore, store } from './redux/store.js';
+import { store } from './redux/store.js';
 import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistedStore}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
